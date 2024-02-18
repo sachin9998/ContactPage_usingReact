3dad0dd2696e9d3ca9377fc875f18e4b1b00134f
Sachin Alam <sachinalam4216@gmail.com>
2024-02-18 13:57:24 +0530

    Pilot

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Navigation from './components/Navigation/Navigation'
+
+function App() {
+
+  return (
+    <div>
+
+      <Navigation></Navigation>
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,22 @@
+
+import style from "./Navigation.module.css"
+
+const Navigation = () => {
+  return (
+    <nav className={`${style.navigation} container`}>
+          
+          <div className="logo">
+              <img src="images/logo.png" alt="Logo"></img>
+          </div>
+
+          <ul>
+              <li>Home</li>
+              <li>About</li>
+              <li>Contact</li>
+          </ul>
+
+    </nav>
+  )
+}
+
+export default Navigation
