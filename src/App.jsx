086ad3de7cd75c2dba9f36f5c901ebb75a633f87
Sachin Alam <sachinalam4216@gmail.com>
2024-02-19 12:39:51 +0530

    Pilot

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import './App.css'
-import Navigation from './components/Navigation/Navigation'
+import "./App.css";
+import ContactForm from "./components/ContactForm/ContactForm";
+
+import ContactHeader from "./components/ContactHeader/ContactHeader";
+import Navigation from "./components/Navigation/Navigation";
 
 function App() {
-
   return (
     <div>
-
-      <Navigation></Navigation>
-
+      <Navigation />
+      <main className="main_container">
+        <ContactHeader />
+        <ContactForm></ContactForm>
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,22 +1,19 @@
-
-import style from "./Navigation.module.css"
+import styles from "./Navigation.module.css";
 
 const Navigation = () => {
   return (
-    <nav className={`${style.navigation} container`}>
-          
-          <div className="logo">
-              <img src="images/logo.png" alt="Logo"></img>
-          </div>
+    <nav className={`${styles.navigation} container`}>
+      <div className="logo">
+        <img src="/images/logo.png" alt="do some coding logo" />
+      </div>
 
-          <ul>
-              <li>Home</li>
-              <li>About</li>
-              <li>Contact</li>
-          </ul>
-
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default Navigation
+export default Navigation;
